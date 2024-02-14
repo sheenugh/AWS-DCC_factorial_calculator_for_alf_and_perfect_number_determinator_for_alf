12f7f7329008145c94b88c900120d55299e546b5
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 23:52:42 +0800

    pseudo code

---
+++ perfect_number _determinator_for_alf.py
@@ -0,0 +1,13 @@
+# ========== PSEUDO CODE ==========
+# - Printing 'PERFECT NUMBER DETERMINATOR FOR ALF'
+# - Checking if the entered integer is a Perfect Number using a loop
+# - Displaying the result if it is a perfect number
+
+
+
+
+
+
+
+
+# NOTE : You can use if-else statement or ternary operator to display the result.
