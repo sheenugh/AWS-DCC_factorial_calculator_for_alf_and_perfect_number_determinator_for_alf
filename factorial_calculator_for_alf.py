671e9af37b3e50b8fa32bea273007a218affccf9
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-14 23:24:42 +0800

    pseudo code

---
+++ factorial_calculator_for_alf.py
@@ -0,0 +1,6 @@
+# ========== PSEUDO CODE ==========
+# - Printing 'FACTORIAL CALCULATOR FOR ALF'
+# - Ask user to input a non-negative number
+# - Calculate the factorial of the user-defined integer using a loop.
+# - Display the factorial result
+
